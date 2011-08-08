48054ca16d61a229d756b136fd84c0fc05af274b
Mike Koss <mckoss@startpad.org>
2011-08-08 13:05:49 -0700

    trim out scratch-specific code

---
+++ src/main.js
@@ -0,0 +1,42 @@
+var clientLib = require('com.pageforest.client');
+
+exports.extend({
+    'main': main
+})
+
+var client;
+var app = {
+    // Loading a document
+    setDoc: function(json) {
+        $('#blob').val(json.blob);
+    },
+
+    // Saving a document
+    getDoc: function() {
+        return {
+            "blob": $('#blob').val()
+        };
+    }
+};
+
+function main() {
+    handleAppCache();
+    client = new clientLib.Client(app);
+    client.addAppBar();
+}
+
+// For offline - capable applications
+function handleAppCache() {
+    if (typeof applicationCache == 'undefined') {
+        return;
+    }
+
+    if (applicationCache.status == applicationCache.UPDATEREADY) {
+        applicationCache.swapCache();
+        location.reload();
+        return;
+    }
+
+    applicationCache.addEventListener('updateready', handleAppCache, false);
+}
+
